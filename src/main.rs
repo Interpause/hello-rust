64610bb10b1f0c2d7b0ea8b663892e5452b1775c
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2022-03-06 16:44:17 +0800

    completed guessing game

--- src/main.rs
+++ src/main.rs
@@ -9,25 +9,38 @@ fn main() {
     let width = msg.chars().count();
 
     let mut writer = BufWriter::new(stdout.lock());
-    say(msg.as_bytes(), width, &mut writer).unwrap();
-    guess();
-}
+    say(msg.as_bytes(), width, &mut writer).expect("stdout error");
 
-fn guess() {
-    println!("Input guess: ");
-    let mut guess = String::new();
     let secret = rand_int();
 
-    stdin().read_line(&mut guess).expect("oof");
-    let guess: u8 = guess.trim().parse().expect("Lmao unsigned 8-bit int only");
-    println!("Input was: {}", guess);
-    println!("Secret was: {}", secret);
+    while !guess(secret) {}
+}
+
+fn guess(secret: u8) -> bool {
+    println!("Input guess: ");
+    let mut guess = String::new();
+
+    stdin().read_line(&mut guess).expect("stdin error");
+
+    let guess: u8 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Lmao unsigned 8-bit int only");
+            return false;
+        }
+    };
+    // println!("Input was: {}", guess);
+    // println!("Secret was: {}", secret);
 
     match guess.cmp(&secret) {
         Ordering::Less => println!("Too small!"),
         Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        Ordering::Equal => {
+            println!("You win!");
+            return true;
+        }
     }
+    return false;
 }
 
 fn rand_int() -> u8 {
